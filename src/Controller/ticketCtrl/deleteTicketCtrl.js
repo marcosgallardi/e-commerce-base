fcbff0401c0d0904536273163e55892fe94e1583
Marcos Gallardi <marcos.gallardi@grupo-sanmartin.com>
2025-12-30 15:20:03 -0300

    avanzando

--- src/Controller/ticketCtrl/deleteTicketCtrl.js
+++ src/Controller/ticketCtrl/deleteTicketCtrl.js
@@ -1,10 +1,10 @@
 const { Tickets } = require("../../DB_Connection");
 
-const deleteTicketCtrl = async ( id) => {
+const deleteTicketCtrl = async (id) => {
     try {
         const data = await Tickets.destroy({ where: { id } });
         if (data) {
-            return data;
+            return data; 
         } else {
             return false;
         }
